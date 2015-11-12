81c652ded16913ab679b67555dc099870c1526c9
DHollander Gregg <greggdh@hotmail.com>
2015-11-12 08:39:56 +0100

    testStory3

--- test/test.js
+++ test/test.js
@@ -12,3 +12,11 @@ PalettoTestCase.prototype.testStory2 = function () {
     e.move("A6");
     assertTrue(e.getCaseBoard(5, 0) === -1);
 };
+
+PalettoTestCase.prototype.testStory3 = function () {
+    var e = new Engine();
+    e.move("A6");
+    assertTrue(e.getCaseBoard(5, 0) === -1);
+    assertTrue(e.getCaseList(0, 5) === 1);
+    assertTrue(e.getNumberToken() === 35);
+};
