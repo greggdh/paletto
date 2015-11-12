c6e8e6d2acb8b977b7a5c40220c1caefe3c1770b
DHollander Gregg <greggdh@hotmail.com>
2015-11-12 08:35:22 +0100

    testStory2 finished

--- src/Engine.js
+++ src/Engine.js
@@ -3,8 +3,29 @@
 var Engine = function () {
 
 // private attributes and methods
-    var board, color;
+    var board, tokenList, color, player, curPlayer;
+    var foreach = function (n1, n2, callback) {
+        var i, j;
+        for (i = 0; i < n1; i++) {
+            for (j = 0; j < n2; j++) {
+                callback(i, j);
+            }
+        }
+    };
+    var initTokenList = function () {
+        var k;
+        for (k = 0; k < 6; k++) {
+            tokenList[k] = new Array (6);
+        }
+        foreach(2, 6, function (i, j) {
+            tokenList[i][j] = 0;
+        });
+    };
     var initBoard = function () {
+        player = {player1 : 0, player2 : 1};
+        curPlayer = player.player1;
+        tokenList = new Array(2);
+        initTokenList();
         color = {black : 0, white : 1, red : 2, green : 3, blue : 4, yellow : 5};
         board = [
             [0,3,1,4,2,1],
@@ -15,14 +36,6 @@ var Engine = function () {
             [5,4,0,2,3,0]
         ];
 
-    };
-    var foreach = function (n, callback) {
-        var i, j;
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < n; j++) {
-                callback(i, j);
-            }
-        }
     };
 
 // public methods
@@ -36,6 +49,17 @@ var Engine = function () {
     };
     this.getCase = function (line, column) {
         return board[line][column];
+    };
+    this.getCaseBoard = function (line, column) {
+        return board[line][column];
+    };
+    this.move = function (token) {
+        var saveColor = null;
+        var column = token.charCodeAt(0) - 65;
+        var line = token.charCodeAt(1) - 49;
+        saveColor = board[line][column];
+        tokenList[curPlayer][saveColor] += 1;
+        board[line][column] = -1;
     };
     initBoard();
 };
