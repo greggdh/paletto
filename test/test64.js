c34b12e6295c45d974e63d9d5e936b74299d757d
DHollander Gregg <greggdh@hotmail.com>
2015-11-30 08:43:31 +0100

    testStory9

--- test/test64.js
+++ test/test64.js
@@ -6,3 +6,32 @@ PalettoTestCase64.prototype.testStory8 = function () {
     var e = new Engine64();
     assertTrue(e.isInit());
 };
+
+Paletto64TestCase.prototype.testStory9 = function () {
+    var e = new Engine();
+    e.moveAll("A1");
+    e.nextPlayer();
+    e.moveAll("B1");
+    e.nextPlayer();
+    e.moveAll("F4");
+    e.nextPlayer();
+    e.moveAll("D6");
+    e.nextPlayer();
+    e.moveAll("E5");
+    e.nextPlayer();
+    e.moveAll("D1");
+    e.nextPlayer();
+    e.moveAll("C6");
+    e.nextPlayer();
+    e.moveAll("A4");
+    e.nextPlayer();
+    e.moveAll("A5");
+    e.nextPlayer();
+    e.moveAll("E2");
+    e.nextPlayer();
+    e.moveAll("D2");
+    e.nextPlayer();
+    e.moveAll("C2");
+    e.nextPlayer();
+    assertTrue(e.getWinner() === 1);
+};
